f25335884085839e64cd94e9a15b7f14eca596dc
sateigen <shannon.teigen@gmail.com>
2016-07-13 16:39:48 -0400

    recursively flipping list of letters

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,21 @@
 import re
 
 def is_palindrome(sentence):
-    # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+    if sentence == []:
+        return
+    is_palindrome(sentence[1:])
+    backwards.append(sentence[0])
+
 
 
 def main():
     user_input = input("Please enter a string. ")
 
+    user_input = user_input.lower()
+
     user_input = re.sub("[^A-Za-z0-9]", "", user_input)
+
+    user_input = list(user_input)
 
     is_palindrome(user_input)
 
