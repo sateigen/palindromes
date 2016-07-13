e41bab77006304907237a989cbc1e1dfcdc3aed5
sateigen <shannon.teigen@gmail.com>
2016-07-13 14:51:32 -0400

    taking out spaces and punctuation from user input.

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,16 @@
+import re
+
 def is_palindrome(sentence):
     # TODO: return True or False if the sentence is or isn't a palindrome
     pass
 
 
 def main():
-    # TODO: put your input/output code here
-    pass
+    user_input = input("Please enter a string. ")
+
+    user_input = re.sub("[^A-Za-z0-9]", "", user_input)
+
+    is_palindrome(user_input)
 
 
 if __name__ == '__main__':
