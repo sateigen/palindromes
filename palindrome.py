4699f3eb609c9f50a9e79ebf09298ed31a8990e1
sateigen <shannon.teigen@gmail.com>
2016-07-13 18:42:45 -0400

    Added main function and is_palindrome function

--- palindrome.py
+++ palindrome.py
@@ -1,23 +1,36 @@
 import re
 
+
 def is_palindrome(sentence):
-    if sentence == []:
-        return
-    is_palindrome(sentence[1:])
-    backwards.append(sentence[0])
+    if len(sentence) < 2:
+        return True
+    if sentence[0] != sentence[-1]:
+        return False
+    else:
+        sentence.pop(0)
+        sentence.pop(-1)
+        is_palindrome(sentence)
+
 
 
 
 def main():
-    user_input = input("Please enter a string. ")
-
-    user_input = user_input.lower()
+    user_input = input("Please enter a string. ").lower()
 
     user_input = re.sub("[^A-Za-z0-9]", "", user_input)
 
     user_input = list(user_input)
 
+    sentence = []
+
+    sentence.append(user_input)
+
     is_palindrome(user_input)
+
+    if is_palindrome(user_input):
+        print("This is a palindrome.")
+    else:
+        print("This is NOT a palindrome")
 
 
 if __name__ == '__main__':
