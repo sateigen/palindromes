600a0fff68d1b4c98e3a911dddbb2f728204af2a
sateigen <shannon.teigen@gmail.com>
2016-07-13 23:18:22 -0400

    Changed recursion to while and if loop for advanced mode. Changed import for palindrome_adv_test.py file.

---
+++ palindrome_adv.py
@@ -0,0 +1,29 @@
+import re
+
+
+def is_palindrome(sentence):
+    if type(sentence) != list:
+        sentence = sentence.lower()
+        sentence = mash_string(sentence)
+    while len(sentence) >= 2:
+        if sentence[0] != sentence[-1]:
+            return False
+        else:
+            sentence.pop(0)
+            sentence.pop(-1)
+    if len(sentence) < 2:
+        return True
+
+
+def mash_string(sentence):
+    return list(re.sub("[^A-Za-z]", "", sentence))
+
+def main():
+    if is_palindrome(input("\nPlease enter a string: ")):
+        print("\nThis is a palindrome.\n")
+    else:
+        print("\nThis is NOT a palindrome.\n")
+
+
+if __name__ == '__main__':
+    main()
