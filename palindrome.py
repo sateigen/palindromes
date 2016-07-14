7401c6cc969c4b74ccd2dfa19a7fcfac089b0ec8
sateigen <shannon.teigen@gmail.com>
2016-07-13 23:09:01 -0400

    added mash_string function back in

--- palindrome.py
+++ palindrome.py
@@ -4,7 +4,7 @@ import re
 def is_palindrome(sentence):
     if type(sentence) != list:
         sentence = sentence.lower()
-        sentence = list(re.sub("[^A-Za-z]", "", sentence))
+        sentence = mash_string(sentence)
     if len(sentence) < 2:
         return True
     if sentence[0] != sentence[-1]:
@@ -14,13 +14,14 @@ def is_palindrome(sentence):
         sentence.pop(-1)
         return is_palindrome(sentence)
 
+def mash_string(sentence):
+    return list(re.sub("[^A-Za-z]", "", sentence))
 
 def main():
-
-    if is_palindrome(input("\nPlease enter a string: ")) == False:
-        print("\nThis is NOT a palindrome.\n")
-    else:
+    if is_palindrome(input("\nPlease enter a string: ")):
         print("\nThis is a palindrome.\n")
+    else:
+        print("\nThis is NOT a palindrome.\n")
 
 
 if __name__ == '__main__':
